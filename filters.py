f2f55751223ac1d336600f6f14f216d1ce86de2a
parisosuch-dev <parisosuch-dev@gmail.com>
2022-11-28 16:30:22 -0800

    mmmm

--- filters.py
+++ filters.py
@@ -3,9 +3,23 @@ import matplotlib.pyplot as plt
 import numpy as np
 import scipy.ndimage as ndi
 
-def remove_gaussian_noise(file: str):
-    arr = np.asarray(skimage.img_as_float(skimage.io.imread(file)))
-    avg = np.mean(arr,0).astype('uint8')
-    skimage.io.imsave(file[:-4]+"_noise_removed.png", avg)
+def clean_image(file: str, save=False):
+    """clean image through using the median to binary close and fill holes."""
+    image_stack = skimage.io.imread(file, as_gray=True)
+    footprint = skimage.morphology.disk(5)
+    filtered = skimage.filters.median(image_stack, footprint=footprint)
+    masked = filtered > skimage.filters.threshold_otsu(filtered)
+    closed = skimage.morphology.binary_closing(masked, footprint=footprint)
+    image_fill = ndi.binary_fill_holes(closed, footprint)
+    if save:
+        skimage.io.imsave(file[:-4]+"_filled_image.png", image_fill)
+    return image_fill
+def contour_image(file: str):
+    # an (x, y, z) image where z is either 0 or 255 values for the binary image
+    image_stack = skimage.io.imread(file)
+    # get segmented image
+    seg_img = clean_image(file)
+    
+    quad_contour_set = plt.contour([])
 
-remove_gaussian_noise("images/phase_color_image.png")
+contour_image('images/phase_color_image.png')
